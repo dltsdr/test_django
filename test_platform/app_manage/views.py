24abc6dafb4110441b47855f3a1c7b15cd725786
dltsdr <852668587@qq.com>
2020-01-31 15:22:43 +0800

    20200131

--- test_platform/app_manage/views.py
+++ test_platform/app_manage/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
 from app_manage.models import Project
-# Create your views here.
+from app_manage.forms import ProjectForm
 
 
 @login_required
@@ -11,5 +12,14 @@ def manage(request):
     return render(request, "project_list.html", {"projects":projects_list})
 
 def add_project(request):
-    print("chuangjianxiangmu")
-    return render(request, "project_add.html", )
+    if request.method == "POST":
+        form = ProjectForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            describe = form.cleaned_data['describe']
+            status = form.cleaned_data['status']
+            Project.objects.create(name=name, describe=describe, status=status)
+        return HttpResponseRedirect("/project/")
+    else:
+        form = ProjectForm()
+    return render(request, 'project_add.html', {'form': form})
